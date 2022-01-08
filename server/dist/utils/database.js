ad8e4aa1f437ddb6384d848debf65debd7162f6e
moneyman01 <tchrocr@gmail.com>
2022-01-08 23:50:20 +0200

    restore

--- server/dist/utils/database.js
+++ server/dist/utils/database.js
@@ -5,7 +5,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = __importDefault(require("mongoose"));
 async function connectDb() {
-    const uri = process.env.MONGO_URI;
+    const uri = process.env.MONGO_URI ||
+        'mongodb+srv://Hlife11:hlifeJJ003nz@hlife.pzl2m.mongodb.net/reminderApp?retryWrites=true&w=majority';
     try {
         await mongoose_1.default.connect(uri);
         console.log('MongoDB connected');
